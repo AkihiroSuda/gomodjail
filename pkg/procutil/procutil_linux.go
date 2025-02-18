af13c039e4109d68afd6c74e1eaad62e96c9f9f4
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2025-02-18 09:58:43 +0900

    CI: run unit tests and linter
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- pkg/procutil/procutil_linux.go
+++ pkg/procutil/procutil_linux.go
@@ -32,3 +32,15 @@ func ReadString(pid int, addr uintptr, bufSize int) (string, error) {
 	}
 	return string(buf[:nilIdx]), nil
 }
+
+func WaitForStopSignal(pid int) (int, unix.Signal, error) {
+	var ws unix.WaitStatus
+	wPid, err := unix.Wait4(pid, &ws, unix.WALL, nil)
+	if err != nil {
+		return 0, 0, err
+	}
+	if !ws.Stopped() {
+		return 0, 0, fmt.Errorf("expected to be stopped (wPid=%d, ws=0x%x)", wPid, ws)
+	}
+	return wPid, ws.StopSignal(), nil
+}
--- pkg/unwinder/unwinder_linux_amd64.go
+++ pkg/unwinder/unwinder_linux_amd64.go
@@ -1,5 +1,3 @@
-//go:build amd64 || arm64
-
 package unwinder
 
 const wordSize = 8
